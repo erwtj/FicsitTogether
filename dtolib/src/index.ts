c8b5a0c357f67e533a8779356ad351e7cf0ff0f6
Dorito009 <Krantje2015@hotmail.com>
2026-02-14 16:26:38 +0100

    Added the first iteration of the char datatypes

--- dtolib/src/index.ts
+++ dtolib/src/index.ts
@@ -11,6 +11,61 @@ export type ChartDTO = {
     chart: string;
 }
 
+export type ItemSpawnerNodeData = {
+    itemClassName: string;
+    outputAmount: number;
+}
+
+export type RecipeNodeData = {
+    recipeClassName: string;
+    summerSloops: number;
+    percentages: number[];
+}
+
+export type PowerNodeData = {
+    recipeClassName: string;
+}
+
+export type outputNodeData = {
+    itemClassName: string;
+    convertToTickets: boolean;
+}
+
+export type NodeDTO = {
+    id: string;
+    type: string;
+    position: {
+        x: number;
+        y: number;
+    };
+    data: ItemSpawnerNodeData | RecipeNodeData | outputNodeData | PowerNodeData; // TODO: Maybe there is a better way?
+    selectedBy: string[]; // TODO: Idk if we want to do it this way?
+}
+
+export type EdgeDTO = {
+    id: string;
+
+    sourceNodeId: string;
+    sourceHandleId: string;
+    targetNodeId: string;
+    targetHandleId: string;
+
+    data: {
+        throughput: number;
+        startHandleType: 'source' | 'target'; // Determines from which handle the node is driven
+        movablePoints?: {
+            id: string;
+            x: number;
+            y: number;
+        }
+    }
+}
+
+export type ChartDataDTO = {
+    nodes: NodeDTO[];
+    edges: EdgeDTO[];
+}
+
 export type DirectoryTreeDTO = {
     id: string;
     name: string;
