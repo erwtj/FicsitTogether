51fcd4f7fd897daf848763f95e62eec9cae20674
Ikno <erwtje013@gmail.com>
2026-03-31 10:40:16 +0200

    Added plausible

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,13 @@ import './index.css'
 import {Auth0Wrapper} from "./auth/auth0.tsx";
 import App from "./App.tsx";
 import {ClientSettingsProvider} from "./context/ClientSettingsContext.tsx";
+import { init } from "@plausible-analytics/tracker";
+
+// TODO: Move domain name to env
+init({
+    domain: 'ficsit-together.com',
+    fileDownloads: true,
+})
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
